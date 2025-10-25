b3850f59f5b88f4077242b312393d61be6e3794d
Maliha Fatima <maliha@localhost.localdomain>
2025-10-25 16:56:19 +0530

    handled Request

--- form.js
+++ form.js
@@ -1,25 +1,38 @@
 const http = require("http");
 const fs = require("fs");
+const querystring=require('querystring')
 
-http.createServer((req, res) => {
+http
+  .createServer((req, res) => {
     fs.readFile("html/form.html", "utf-8", (error, data) => {
-        if (error) {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("Internal server error");
-            return;
-        }
+      if (error) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal server error");
+        return;
+      }
 
-        if (req.url == "/") {
+      if (req.url == "/") {
+        res.write(data);
+        // use res.end here
+      } else if (req.url == "/submit") {
+        let dataBody = [];
+        req.on("data", (chunks) => {
+          dataBody.push(chunks);
+        });
+        req.on('end',()=>{
+            let rawData=Buffer.concat(dataBody).toString();
+            let readableData=querystring.parse(rawData)
+            console.log("FormData:",readableData);
             res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(data);  // use res.end here
-        } else if (req.url == "/submit") {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end("<h1>Form Submitted Successfully</h1>"); // use res.end here too
-        } else {
-            res.writeHead(404, { "Content-Type": "text/plain" });
-            res.end("Page not found");
-        }
+            res.end(`<h1>Thanks ${readableData.name}! We got your email: ${readableData.email}</h1>`)
+        })
+        
+      } else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Page not found");
+      }
     });
-}).listen(8080, () => {
+  })
+  .listen(8080, () => {
     console.log("Server is running on http://localhost:8080");
-});
+  });
